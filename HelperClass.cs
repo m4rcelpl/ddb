b81d26b8df08219287bc7260c4e898c498e46d26
m4rcel <m4rcelpl@outlook.com>
2019-08-11 12:54:54 +0200

    Change name and add new method to calculate time Delay

--- HelperClass.cs
+++ HelperClass.cs
@@ -8,7 +8,7 @@ namespace ddb
     /// reference
     /// https://loune.net/2017/06/running-shell-bash-commands-in-net-core/
     /// </summary>
-    public static class ShellHelper
+    public static class HelperClass
     {
         public static string Bash(this StringBuilder cmd)
         {
@@ -33,10 +33,22 @@ namespace ddb
 
                 return String.IsNullOrEmpty(standardOutput) ? standardError : standardOutput;
             }
-            catch (System.Exception)
+            catch (Exception)
             {
                 throw;
             }
+        }
+        public static int GetMilisecund(int hour, int minute)
+        {
+            DateTime firstStart = DateTime.Today.AddHours(hour).AddMinutes(minute);
+            if (hour < DateTime.Now.Hour)
+            {
+                firstStart = firstStart.AddDays(1);
+            }
+
+            TimeSpan span = firstStart - DateTime.Now;
+
+            return (int)span.TotalMilliseconds;
         }
     }
 }
