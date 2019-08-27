dd3e25962699365dcc77afce8e60b55be6bf7ef8
m4rcel <m4rcelpl@outlook.com>
2019-08-27 20:40:47 +0200

    First commit

---
+++ build.bat
@@ -0,0 +1,10 @@
+docker buildx build -f .\Dockerfile -t m4rcel/ddb -t m4rcel/ddb:1 -t m4rcel/ddb:1.1 -t m4rcel/ddb:latest .
+docker push m4rcel/ddb
+docker push m4rcel/ddb:1
+docker push m4rcel/ddb:1.1
+docker push m4rcel/ddb:latest
+
+docker build -f .\Dockerfile.arm -t m4rcel/ddb:arm7 -t m4rcel/ddb:1-arm7 -t m4rcel/ddb:1.1-arm7 .
+docker push m4rcel/ddb:arm7
+docker push m4rcel/ddb:1-arm7
+docker push m4rcel/ddb:1.1-arm7
