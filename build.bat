96aacd9631abf2dcc79ef8552091fe2a38b86e61
m4rcel <m4rcelpl@outlook.com>
2019-08-29 20:41:24 +0200

    add --no-cache

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ docker push m4rcel/ddb:1
 docker push m4rcel/ddb:1.1
 docker push m4rcel/ddb:latest
 
-docker build -f .\Dockerfile.arm -t m4rcel/ddb:arm7 -t m4rcel/ddb:1-arm7 -t m4rcel/ddb:1.1-arm7 .
+docker build --no-cache -f Dockerfile.arm -t m4rcel/ddb:arm7 -t m4rcel/ddb:1-arm7 -t m4rcel/ddb:1.1-arm7 .
 docker push m4rcel/ddb:arm7
 docker push m4rcel/ddb:1-arm7
 docker push m4rcel/ddb:1.1-arm7
