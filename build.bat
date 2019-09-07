b3a4595fc3882ccc4eb9acc00e7dc5d43f5aa7e9
m4rcel <m4rcelpl@outlook.com>
2019-09-07 12:11:39 +0200

    Delete unuse property

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
 docker buildx build -f Dockerfile -t m4rcel/ddb -t m4rcel/ddb:2 -t m4rcel/ddb:latest .
-docker push m4rcel/ddb
 docker push m4rcel/ddb:2
 docker push m4rcel/ddb:latest
 
 docker buildx build -f Dockerfile.arm --platform linux/arm/v7 -t m4rcel/ddb:arm7 -t m4rcel/ddb:2-arm7 .
-docker push m4rcel/ddb:arm7
 docker push m4rcel/ddb:2-arm7
