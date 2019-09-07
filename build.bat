622d5fa474aaf8476bb6bd4b1b21f99317f68e79
m4rcel <m4rcelpl@outlook.com>
2019-09-07 12:09:16 +0200

    Add buildx

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-docker build -f Dockerfile -t m4rcel/ddb -t m4rcel/ddb:2 -t m4rcel/ddb:latest .
+docker buildx build -f Dockerfile -t m4rcel/ddb -t m4rcel/ddb:2 -t m4rcel/ddb:latest .
 docker push m4rcel/ddb
 docker push m4rcel/ddb:2
 docker push m4rcel/ddb:latest
 
-docker build -f Dockerfile.arm --platform linux/arm/v7 -t m4rcel/ddb:arm7 -t m4rcel/ddb:2-arm7 .
+docker buildx build -f Dockerfile.arm --platform linux/arm/v7 -t m4rcel/ddb:arm7 -t m4rcel/ddb:2-arm7 .
 docker push m4rcel/ddb:arm7
 docker push m4rcel/ddb:2-arm7
