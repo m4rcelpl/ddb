8d357679b67d33c4c891cb5e0b01bc1e29bfad10
m4rcel <m4rcelpl@outlook.com>
2019-09-28 17:40:49 +0200

    Version bump

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-docker buildx build -f Dockerfile -t m4rcel/ddb -t m4rcel/ddb:3 -t m4rcel/ddb:latest .
-docker push m4rcel/ddb:3
+docker buildx build -f Dockerfile -t m4rcel/ddb -t m4rcel/ddb:3.1 -t m4rcel/ddb:latest .
+docker push m4rcel/ddb:3.1
 docker push m4rcel/ddb:latest
 
-docker buildx build -f Dockerfile.arm --platform linux/arm/v7 -t m4rcel/ddb:arm7 -t m4rcel/ddb:3-arm7 .
-docker push m4rcel/ddb:3-arm7
+docker buildx build -f Dockerfile.arm --platform linux/arm/v7 -t m4rcel/ddb:arm7 -t m4rcel/ddb:3.1-arm7 .
+docker push m4rcel/ddb:3.1-arm7
